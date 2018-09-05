06f83751d1fcdb62aeb2a178b58431c12923baa7
chloe alcorn <ca.alcorn@outlook.com>
2018-09-05 13:33:53 +0100

    find film by title added

--- models/cinema.js
+++ models/cinema.js
@@ -8,5 +8,13 @@ Cinema.prototype.filmTitles = function () {
 return this.films.map(film => film.title);
 };
 
+Cinema.prototype.findFilm = function (string) {
+  return this.films.find((film) => film.title === string);
+};
+
+// needs a condition that is true or false which is what === string is
+// string is the parameter passed in 
+
+
 
 module.exports = Cinema;
